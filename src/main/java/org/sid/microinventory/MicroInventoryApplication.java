e2d0ea871f4a91c39b3875fd7c71512941211f80
Zakaria Bounou <zakariabounouu@gmail.com>
2025-01-04 11:26:53 +0100

    first ini

---
+++ src/main/java/org/sid/microinventory/MicroInventoryApplication.java
@@ -0,0 +1,13 @@
+package org.sid.microinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/microinventory/MicroInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.microinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
